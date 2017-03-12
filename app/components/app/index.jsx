222c415364edfb29a052e34a5a45d73d45cde244
Jenny Knuth <jknuth@n.io>
2017-03-11 21:18:00 -0700

    add favicon and header

--- app/components/app/index.jsx
+++ app/components/app/index.jsx
@@ -2,19 +2,29 @@ import React from 'react';
 import Steps from 'components/app/steps';
 import Tweet from 'components/app/tweet';
 import layout from 'theme/layout.scss';
+import header from 'theme/header.scss';
 
 const MainApp = (props) => {
   return (
-  <div className={layout.app}>
-    <h1>{props.children.props.route.title}</h1>
-    <div className={layout.container}>
-      <div className={layout.bigPanel}>
-        { props.children }
+  <div>
+    <header className={header.header}>
+      <div>
+        <img className={header.logo} src='img/niologo_white.png' />
+        <div className={header.name}>school</div>
       </div>
-      <div className={layout.smallPanel}>
-        <Steps activeRoute={props.children.props.route.path} />
-        <div className={layout.paper}>
-          <Tweet />
+      <div className={header.info}>Questions? Call XXX-XXX-XXX MF 9–5 MST</div>
+    </header>
+    <div className={layout.app}>
+      <h1>{props.children.props.route.title}</h1>
+      <div className={layout.container}>
+        <div className={layout.bigPanel}>
+          { props.children }
+        </div>
+        <div className={layout.smallPanel}>
+          <Steps activeRoute={props.children.props.route.path} />
+          <div className={layout.paper}>
+            <Tweet />
+          </div>
         </div>
       </div>
     </div>
