118d6258e30dd92379bec370bb38e6a96a4a1d17
Jenny Knuth <jknuth@n.io>
2017-03-13 15:10:49 -0600

    add Link to home button

--- app/initialize.jsx
+++ app/initialize.jsx
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <Router history={history}>
-          <Route path="/labtest" component={MainApp}>
+          <Route path="/" component={MainApp}>
             <IndexRoute component={Intro} title="Welcome" />
             <Route path="lab1" component={Weather} title="Weather Lab" />
             <Route path="lab2" component={FanLab} title="Fan Lab" />
