c779357cf80590e5130a77ebf4b935a14899df8b
Jenny Knuth <jknuth@n.io>
2017-03-22 10:20:32 -0600

    add windflow wave and styles

--- app/components/flow/flow-windspeed.jsx
+++ app/components/flow/flow-windspeed.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import classNames from 'classnames';
 
 import flow from 'theme/flow.scss';
 
 const FlowWindSpeed = (props) => {
-  const arrow = props.flow < 2 ? 'arrowUp' : 'arrowDown';
+  const getWindClasses = height => classNames(
+    flow.wave,
+    props.flow < 2 && flow[height]
+  );
+
   return (
     <div className={flow.speed}>
       <div className={flow.fanWindSpeed}>
-        <div className={flow.title} >Wind Speed</div>
-        <div className={flow[arrow]} />
+        <svg className={getWindClasses('thirty')} xmlns="http://www.w3.org/2000/svg" width="300" height="60" viewBox="0 0 300 60">
+          <path strokeWidth="18" fill="none"
+            d="M0,40
+            c30,0 45,-20 75,-20
+            c30,0  45,20  75,20
+            c30,0 45,-20 75,-20
+            c30,0  45,20  75,20"
+          />
+        </svg>
+        <svg className={getWindClasses('fifty')} xmlns="http://www.w3.org/2000/svg" width="300" height="60" viewBox="0 0 300 60">
+          <path strokeWidth="18" fill="none"
+            d="M0,40
+            c30,0 45,-20 75,-20
+            c30,0  45,20  75,20
+            c30,0 45,-20 75,-20
+            c30,0  45,20  75,20"
+          />
+        </svg>
+        <svg className={getWindClasses('seventy')} xmlns="http://www.w3.org/2000/svg" width="300" height="60" viewBox="0 0 300 60">
+          <path strokeWidth="18" fill="none"
+            d="M0,40
+            c30,0 45,-20 75,-20
+            c30,0  45,20  75,20
+            c30,0 45,-20 75,-20
+            c30,0  45,20  75,20"
+          />
+        </svg>
       </div>
     </div>
   );
