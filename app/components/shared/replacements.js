5134281c3f198d4f682502731bb3d7d293a43ab3
mattdodge <matt@n.io>
2017-03-29 19:51:23 -0600

    Use lab number instead of fixed mobile URL

--- app/components/shared/replacements.js
+++ app/components/shared/replacements.js
@@ -1,7 +1,9 @@
 import config from 'config';
 
 const replacements = {
-  MOBILE_URL: config.MOBILE_URL,
+  MOBILE_URL: `https://nio.school/mobile${config.MOBILE_URL}`,
+  PI_INSTANCE: `pi${config.MOBILE_URL}`,
+  CLOUD_INSTANCE: `cloud${config.MOBILE_URL}`,
 };
 
 export default replacements;
