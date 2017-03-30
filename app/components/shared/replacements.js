a9abea7a217e8bc8921fa2e80338c2932042eaf2
mattdodge <matt@n.io>
2017-03-29 19:55:21 -0600

    use LAB_NUMBER not MOBILE_URL

--- app/components/shared/replacements.js
+++ app/components/shared/replacements.js
@@ -1,9 +1,9 @@
 import config from 'config';
 
 const replacements = {
-  MOBILE_URL: `https://nio.school/mobile${config.MOBILE_URL}`,
-  PI_INSTANCE: `pi${config.MOBILE_URL}`,
-  CLOUD_INSTANCE: `cloud${config.MOBILE_URL}`,
+  MOBILE_URL: `https://nio.school/mobile${config.LAB_NUMBER}`,
+  PI_INSTANCE: `pi${config.LAB_NUMBER}`,
+  CLOUD_INSTANCE: `cloud${config.LAB_NUMBER}`,
 };
 
 export default replacements;
