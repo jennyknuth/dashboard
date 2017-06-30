baa9533d1670ee6bedbfa17d0b62c645180b9f75
Jenny Knuth <jknuth@n.io>
2017-06-30 09:57:00 -0600

    base navigation on router location and push history

--- app/components/app/index.jsx
+++ app/components/app/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router';
+
+import { Link, withRouter, browserHistory } from 'react-router';
 import List from 'react-nuik/lib/components/list';
 import classNames from 'classnames';
 
@@ -7,28 +8,18 @@ import layout from 'theme/layout.scss';
 import header from 'theme/header.scss';
 import nav from 'theme/nav.scss';
 
-class MainApp extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      selected: undefined,
-    };
-    this.selectedNav = this.selectedNav.bind(this);
-  }
-
-  selectedNav = (e) => {
-    const selection = this.state.selected === e.target.name ? undefined : e.target.name;
-    this.setState({ selected: selection });
+const MainApp = (props) => {
+  const selectedNav = (e) => {
+    browserHistory.push(`/${e.target.name}`);
   };
 
-  render() {
-    const selectedClasses = (element) => {
-      return classNames(
-        this.state.selected === element ? nav.active : nav.item,
-      );
-    };
+  const selectedClasses = (element) => {
+    return classNames(
+      props.location.pathname === `/${element}` ? nav.active : nav.item,
+    );
+  };
 
-    return (
+  return (
     <div>
       <header className={header.header}>
         <Link className={header.link} to="/">
@@ -38,31 +29,29 @@ class MainApp extends React.Component {
       <div className={layout.frame}>
         <nav className={nav.bar}>
           <List variant='none'>
-            <Link className={selectedClasses('industrial')} to="/industrial" name='industrial' onClick={(e) => this.selectedNav(e)}>
+            <Link className={selectedClasses('industrial')} to="/industrial" name='industrial' onClick={(e) => selectedNav(e)}>
               Industrial
             </Link>
-            <Link className={selectedClasses('product')} to="/product" name='product' onClick={(e) => this.selectedNav(e)}>
+            <Link className={selectedClasses('product')} to="/product" name='product' onClick={(e) => selectedNav(e)}>
               Product
             </Link>
-            <Link className={selectedClasses('agriculture')} to="/agriculture" name='agriculture' onClick={(e) => this.selectedNav(e)}>
+            <Link className={selectedClasses('agriculture')} to="/agriculture" name='agriculture' onClick={(e) => selectedNav(e)}>
               Agriculture
             </Link>
-            <Link className={selectedClasses('dashboard')} to="/dashboard" name='dashboard' onClick={(e) => this.selectedNav(e)}>
+            <Link className={selectedClasses('dashboard')} to="/dashboard" name='dashboard' onClick={(e) => selectedNav(e)}>
               Socinio
             </Link>
           </List>
         </nav>
         <div className={layout.app}>
-          <h1 className={layout.pageTitle}>{this.props.children.props.route.title}</h1>
+          <h1 className={layout.pageTitle}>{props.children.props.route.title}</h1>
           <div>
-            { this.props.children }
+            { props.children }
           </div>
         </div>
       </div>
     </div>
-    );
+  );
+};
 
-  }
-}
-
-export default MainApp;
+export default withRouter(MainApp);
