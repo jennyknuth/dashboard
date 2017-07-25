f393aa898f3e80bfb66ed63b859a7e531e69f30d
Kylie Dale <kylie.dale@colorado.edu>
2017-07-25 09:20:46 -0600

    Remove time series graph, add clock back

--- app/components/dashboard/index.jsx
+++ app/components/dashboard/index.jsx
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import classNames from 'classnames';
 
 import BarGraph from 'components/shared/bar-graph';
-import TimeSeriesGraph from 'components/shared/area-graph';
 import Clock from 'components/shared/clock';
 import DisplayList from 'components/shared/display-list';
 import Statistic from 'components/shared/statistic';
@@ -31,23 +30,25 @@ const Dashboard = (props) => {
         <Statistic label='' unit='' value={props.jazz_applicants_count.applicants} />
       </div>
       <div className={layout.paper}>
-        <h2>Kitchen Stream</h2>
-        <img src="http://65.112.205.134:8282/stream/video.mjpeg" width="250" height="200" />
-      </div>
-      <div className={layout.paper}>
-        <h2>Cups of Coffee</h2>
-        <h3>Made Today</h3>
-        <Statistic value={props.kitchen_coffee_count.coffee_count} />
-      </div>
-      <div className={barGraphClasses()}>
-        <h2>Number of Employees</h2>
-        <h3 style={{color: 'white'}}>.</h3>
-        { <TimeSeriesGraph vals={props.timely_employees_continuous && props.timely_employees_continuous} />}
+        <Clock
+          day={props.time && props.time.day}
+          month={props.time && props.time.month}
+          year={props.time && props.time.year}
+          weekday={props.time && props.time.weekday}
+          time={props.time && props.time.currentTime}
+        />
       </div>
       <div className={barGraphClasses()}>
         <h2>Q3</h2>
         <h3>Hours Worked</h3>
         { <BarGraph data={props.timely_hours_ordinal && props.timely_hours_ordinal} />}
+      </div>
+      <div className={layout.paper}>
+      </div>
+      <div className={layout.paper}>
+        <h2>Cups of Coffee</h2>
+        <h3>Made Today</h3>
+        <Statistic value={props.kitchen_coffee_count.coffee_count} />
       </div>
     </div>
   );
