c6baf4b79e3778473d40bdbd01b9822cb1a3e045
Jenny Knuth <jknuth@n.io>
2017-11-10 13:55:24 -0700

    add announcement array, get ready for reset signal

--- app/reducers/today.jsx
+++ app/reducers/today.jsx
@@ -1,8 +1,28 @@
+import * as d3 from 'd3';
+
 const defaultState = {
   reminders: [],
   announcements: [],
   birthdays: [],
 };
+
+const randomAnnouncements = [
+  ['Bill is running a bit late today.', 'Run, Bill, run!'],
+  ['Today will be followed by tomorrow.'],
+  ['There is coffee in the kitchen.'],
+  ['Somewhere near you, there is a nio system is running.'],
+  ['You arrived early this morning!'],
+  ['Hello from your friendly nio service.'],
+  ['You are not alone.'],
+  ['Thanks for turning me on.', 'Get Bill'],
+  ['Keanu Leaves is alive and well!'],
+  ['I am looking for Bill.', 'If you have seen him, could you let him know?'],
+  ['I\'m afraid I can\'t do that, Dave']
+];
+
+const announcement = d3.shuffle(randomAnnouncements)[0];
+
+console.log(announcement);
 
 const today = (state, action) => {
   if (state === undefined) {
@@ -15,15 +35,19 @@ const today = (state, action) => {
         reminders: action.data.posts,
       };
     case 'ANNOUNCEMENTS':
+
       return {
         ...state,
-        announcements: action.data.posts && action.data.posts.length > 0 ? action.data.posts : ['Bill is running a bit late today.', 'Run, Bill, run!'],
+        announcements: action.data.posts && action.data.posts.length > 0 ? action.data.posts : announcement,
       };
     case 'BIRTHDAYS':
       return {
         ...state,
         birthdays: action.data.posts,
       };
+    case 'RESET':
+      window.location.reload();
+      return;
     default:
       return state;
   }
